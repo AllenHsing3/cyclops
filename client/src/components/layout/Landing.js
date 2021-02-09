9f887efd74edab3c149ff9d996135065f763919d
AllenHsing3 <allen.hsing@gmail.com>
2021-02-09 14:00:55 -0800

    Landing changes

--- client/src/components/layout/Landing.js
+++ client/src/components/layout/Landing.js
@@ -11,26 +11,27 @@ const Landing = ({ isAuthenticated }) => {
     return(
         <section className="landing">
           <div className="landing-inner">
-            <div className="buttons">
-              <p style={{fontSize:"1.5rem", marginBottom:"1vh"}}>A virtual watch box to keep track of your collection, or to share it with others.</p>
-              <Link to="/user/register" className="btn btn-primary btn-pill">Sign Up</Link>
+            <div className="buttons" style={{paddingTop:"3vh"}}>
+              <p style={{fontSize:"1.5rem", marginBottom:"1vh"}}>A virtual watch box to keep track of your collection, and to share it with others.</p>
+              <Link to="/user/register" className="btn btn-light btn-pill">Sign Up</Link>
               <Link to="/user/login" className="btn btn-light btn-pill">Login</Link>
             </div>
         </div>
         <div className="landing-inner">
-          <div className="main-container" style={{flexDirection:"row"}}>
+
             <div className="landing-about">
-              <ul className='text-primary' style={{fontSize:"2rem"}}>
+              <ul className='text-primary' style={{fontSize:"1.2rem"}}>
                 <li>-Add your watches into your showcase</li>
                 <br/>
                 <li>-Customize your profile to be seen by other collectors</li>
               </ul>
             </div>
-          <video width="60%" height="auto" autoplay muted loop id="vid">     
+            <div className="vid-container">
+          <video width="100%" height="auto" autoplay muted loop id="vid">     
           <source src="https://slate.textile.io/ipfs/bafybeibbkvprlad4pu5mmowmentuzl5gxyze44wyu352rpsb3youo3ikmu"></source>         
             </video>
+            </div>
           </div>
-        </div>
       </section>  
     )
 }
